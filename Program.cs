f412aa2dd457917b692e6477f918cfaafa62bfb7
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2025-04-01 20:44:32 -0300

    tarefa JurosSimples finalizada

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿//Tarefa JurosSimple
+Console.Clear();
+
+double c, j, i, t, m;
+Console.Write("Capital: ");
+c = Convert.ToDouble(Console.ReadLine()!);
+Console.Write("Juros: ");
+i = Convert.ToDouble(Console.ReadLine()!);
+Console.Write("Meses: ");
+t = Convert.ToDouble(Console.ReadLine()!);
+
+j = c * i * t / 100;
+m = c + j;
+Console.Write("Seus juros são de: ");
+    Console.WriteLine($"{j}");
+Console.Write("Seu montante é de: ");
+    Console.Write($"{m}");
